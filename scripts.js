32d05350f80deafc88d9cb4628f0e050e9d59719
lrmoen92 <30440416+lrmoen92@users.noreply.github.com>
2018-05-26 22:07:58 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ function addFour() {
 	console.log(fourString + fourInt);
 	console.log("Yay, you logged a number, but 4 + 4 isn't 44...");
 	console.log(fourInt + fourInt);
+	$("h2").append("<p> 4 + 4 = 44 </p>");
 	return true;
 }
 
