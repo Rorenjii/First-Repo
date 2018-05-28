48894e9c027ccb6a81dc0abc43421b1482902bd1
lrmoen92 <30440416+lrmoen92@users.noreply.github.com>
2018-05-28 15:20:42 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -23,3 +23,49 @@ function areStringsForNoobs() {
 		return true;
 	}
 }
+
+function capitalString(){
+    var input = document.getElementById("stringTest").value;
+    var lowerCaseWords = input.toLowerCase().split(" ");
+    return (function(words){
+        var correctString = "";
+        for (var i = 0; i < words.length; i++) {
+            var word = words[i];
+            var letters = word.split("");
+            var correctWord = (function(letters){
+                var correctWord = "";
+                for (var j = 0; j < letters.length; j++) {
+                    var letter = letters[j];
+                    var correctLetter = "";
+                    if (j === 0) {
+                        correctLetter = letter.toUpperCase();
+                    } else {
+                        correctLetter = letter;
+                    }
+                    correctWord += correctLetter;
+                }
+                return correctWord;
+            })(letters);
+            correctString += correctWord;
+            if (i !== words.length - 1) {
+                correctString += " ";
+            }
+
+        }
+        printResult(correctString);
+    })(lowerCaseWords);
+}
+
+function titleCase() {
+    var str = document.getElementById("stringTest").value;
+    var lower = str.toLowerCase(0);
+    var split = lower.split(" ");
+    for (var i = 0; i < split.length; i++) {
+        split[i] = split[i].charAt(0).toUpperCase() + split[i].replace(split[i].charAt(0), "");
+    }
+    printResult(split.join(" "));
+}
+
+function printResult(str){
+    document.getElementById("#result").innerHTML = str;
+}
